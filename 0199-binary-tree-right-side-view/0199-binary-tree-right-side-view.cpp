3a5cc8b87cd3da172f6db89956c2ba2a5a69f6a3
wani0502 <wanirathaur24@gmail.com>
2026-07-08 12:33:09 +0545

    Time: 0 ms (100%), Space: 15 MB (63.86%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve(TreeNode* root, int level, vector<int> &ans) {
+
+        if(root == NULL)
+            return;
+
+        if(level == ans.size())
+            ans.push_back(root->val);
+
+        solve(root->right, level + 1, ans);
+        solve(root->left, level + 1, ans);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+
+        vector<int> ans;
+        solve(root, 0, ans);
+        return ans;
+    }
+};
