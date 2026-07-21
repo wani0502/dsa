382c9868ab9c72a259d345e30cd3891de625119c
wani0502 <wanirathaur24@gmail.com>
2026-07-21 11:07:54 +0545

    Time: 12 ms (6.54%), Space: 33.2 MB (22.79%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int g = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            g = gcd(g, nums[i]);
+        }
+
+        return g == 1;
+    }
+};
