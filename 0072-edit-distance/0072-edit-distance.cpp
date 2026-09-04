b6f1b353b91f0b878f6b9aa8dec0955bb939de23
wani0502 <wanirathaur24@gmail.com>
2026-09-04 22:25:30 +0545

    Time: 0 ms (100%), Space: 10.3 MB (98.52%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,38 +4,43 @@ public:
         int m = word1.size();
         int n = word2.size();
         
-        // dp[i][j] represents the minimum operations to convert 
-        // the first i characters of word1 to the first j characters of word2.
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        // We only need a 1D array of size n + 1
+        vector<int> dp(n + 1, 0);
         
-        // Base case 1: word2 is empty, we must delete all characters in word1
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = i;
-        }
-        
-        // Base case 2: word1 is empty, we must insert all characters of word2
+        // Initialize base case: converting an empty word1 to word2 takes j insertions
         for (int j = 0; j <= n; j++) {
-            dp[0][j] = j;
+            dp[j] = j;
         }
         
-        // Fill the DP table iteratively
         for (int i = 1; i <= m; i++) {
+            // 'prev' stores the value of dp[i-1][j-1] (the top-left diagonal)
+            int prev = dp[0]; 
+            
+            // Base case for the current row: converting word1 of length i to empty word2 takes i deletions
+            dp[0] = i; 
+            
             for (int j = 1; j <= n; j++) {
-                // If the current characters match, carry over the previous diagonal value
+                // Store the current dp[j] (which acts as the 'top' cell for this iteration) 
+                // before we overwrite it, so it can be used as 'prev' in the next iteration.
+                int temp = dp[j]; 
+                
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                // If they don't match, take the minimum of the 3 possible operations + 1
-                else {
-                    dp[i][j] = 1 + min({
-                        dp[i][j - 1],    // Insert
-                        dp[i - 1][j],    // Delete
-                        dp[i - 1][j - 1] // Replace
+                    // Match: carry over the diagonal value
+                    dp[j] = prev;
+                } else {
+                    // No match: take the minimum of Insert, Delete, and Replace
+                    dp[j] = 1 + min({
+                        dp[j - 1], // Insert (left cell, already updated for current row)
+                        dp[j],     // Delete (top cell, not yet updated)
+                        prev       // Replace (top-left diagonal)
                     });
                 }
+                
+                // Update 'prev' to act as the diagonal for the next column (j+1)
+                prev = temp; 
             }
         }
         
-        return dp[m][n];
+        return dp[n];
     }
 };
