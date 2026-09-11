00942ea17f354ec2488db9c819da013730aed6ce
wani0502 <wanirathaur24@gmail.com>
2026-09-11 23:46:18 +0545

    Time: 0 ms (100%), Space: 15.5 MB (86.31%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0, 0}});
-        while (!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(node -> val);
-            if (node -> left) {
-                todo.push({node -> left, {x - 1, y + 1}});
+        map<int, vector<pair<int, int>>> mp;
+        queue<tuple<TreeNode*, int, int>> q;
+        q.push({root, 0, 0});
+        while (!q.empty()) {
+            auto [node, row, col] = q.front();
+            q.pop();
+            if (node == nullptr) {
+                continue;
             }
-            if (node -> right) {
-                todo.push({node -> right, {x + 1, y + 1}});
-            }
+            mp[col].push_back({row, node->val});
+            q.push({node->left, row + 1, col - 1});
+            q.push({node->right, row + 1, col + 1});
         }
         vector<vector<int>> ans;
-        for (auto p : nodes) {
-            vector<int> col;
-            for (auto q : p.second) {
-                col.insert(col.end(), q.second.begin(), q.second.end());
+        for (auto &entry : mp) {
+            auto &v = entry.second;
+            sort(v.begin(), v.end(), [](auto &a, auto &b) {
+                if (a.first != b.first)
+                    return a.first < b.first;
+
+                return a.second < b.second;
+            });
+            vector<int> column;
+            for (auto &p : v) {
+                column.push_back(p.second);
             }
-            ans.push_back(col);
+            ans.push_back(column);
         }
         return ans;
     }
