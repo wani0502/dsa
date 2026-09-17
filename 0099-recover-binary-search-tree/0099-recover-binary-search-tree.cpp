8175f6461f2c1fb1511a032bc2865c89c7827343
wani0502 <wanirathaur24@gmail.com>
2026-09-17 17:14:47 +0545

    Time: 0 ms (100%), Space: 60.7 MB (78.4%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,45 +1,29 @@
 class Solution {
 private:
     TreeNode* first = nullptr;
-    TreeNode* second = nullptr;
-    TreeNode* prev = nullptr;
+    TreeNode* middle = nullptr;
+    TreeNode* last= nullptr;
 
     void inorder(TreeNode* root) {
         if (!root) return;
-        
-        // Traverse the left subtree
-        inorder(root->left);
-        
-        // Process the current node
-        if (prev != nullptr && root->val < prev->val) {
-            // If this is the first time we see a violation, 
-            // the first swapped node is the previous node.
-            // We also tentatively set the second node to the current one 
-            // (in case the swapped nodes are adjacent).
+        inorder(root->left);  
+        if (last != nullptr && root->val < last->val) {  
             if (first == nullptr) {
-                first = prev;
-                second = root;
-            } else {
-                // If it's the second violation, the second swapped node is the current one.
-                second = root;
+                first = last;
+                middle= root;
+            } else {    
+                middle= root;
             }
-        }
-        
-        // Update previous node to current before moving to right subtree
-        prev = root;
-        
-        // Traverse the right subtree
+        }   
+        last = root;
         inorder(root->right);
     }
 
 public:
     void recoverTree(TreeNode* root) {
-        // Find the two swapped nodes
         inorder(root);
-        
-        // Swap their values to recover the BST
-        if (first && second) {
-            std::swap(first->val, second->val);
+        if (first && middle) {
+            std::swap(first->val, middle->val);
         }
     }
 };
